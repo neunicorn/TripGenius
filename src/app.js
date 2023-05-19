dbc1adf3143fa2bc4e910c795d5664e6e18e30ff
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-05-19 09:57:01 +0700

    make a server, add dependencies/libraries. and make a structure folder

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const http = require("http");
+const authRoutes = require("./routes/api/v1/auth.js");
+
+const app = express();
+const env = dotenv.config().parsed;
+
+//parsing application/json
+app.use(express.json());
+
+app.use("/v1/auth", authRoutes);
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  // next(err);
+  res.status(404).json({
+    message: "Not Found",
+    status: 404,
+  });
+});
+
+const PORT = env.APP_PORT;
+const HOST = env.APP_HOST;
+
+const server = http.createServer(app);
+console.log(`Server running at http://${HOST}:${PORT}/`);
+server.listen(PORT, HOST);
