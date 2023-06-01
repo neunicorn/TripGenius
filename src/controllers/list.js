6a6792c5e6e8d4521c963c286cd8ea30914ef6be
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-06-01 22:28:22 +0700

    done getdetaillist probably

--- src/controllers/list.js
+++ src/controllers/list.js
@@ -18,7 +18,22 @@ class List {
     }
   }
 
-  async getDetailList(req, res) {}
+  async getDetailList(req, res) {
+    try {
+      const { id } = req.body;
+      const result = await ListModel.getDetailList(id);
+      return res.status(200).json({
+        status: true,
+        message: "GET_DETAIL_LIST_SUCCESS",
+        data: result,
+      });
+    } catch (err) {
+      return res.status(err.code || 500).json({
+        status: false,
+        message: err.message,
+      });
+    }
+  }
 }
 
 module.exports = new List();
