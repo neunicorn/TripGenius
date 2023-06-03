7607465b9fd845898bd6f667ad730729ec86fca4
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-06-03 14:48:39 +0700

    menambahkan addf list pada controller

--- src/controllers/list.js
+++ src/controllers/list.js
@@ -33,6 +33,31 @@ class List {
       });
     }
   }
+  async addList(req, res) {
+    try {
+      const { wisata_fk, hotel_fk, tranportation_fk } = req.body;
+      const uid_fk = req.jwt.id;
+      const status = "true";
+      const data = {
+        uid_fk,
+        wisata_fk,
+        hotel_fk,
+        tranportation_fk,
+        status,
+      };
+      const result = await ListModel.addList(data);
+      return res.status(200).json({
+        status: true,
+        message: "ADD_LIST_SUCCESS",
+        data: result,
+      });
+    } catch (err) {
+      return res.status(err.code || 500).json({
+        status: false,
+        message: err.message,
+      });
+    }
+  }
 }
 
 module.exports = new List();
