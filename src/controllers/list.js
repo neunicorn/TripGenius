42aae0b48bdec10597e34c1b41fa8b08ceba0c60
Muhamad Zulfan <73164719+neunicorn@users.noreply.github.com>
2023-06-07 00:33:44 +0700

    memperbaikin history endpoints

--- src/controllers/list.js
+++ src/controllers/list.js
@@ -20,7 +20,13 @@ class List {
   async getDetailList(req, res) {
     try {
       const { id } = req.body;
-      const result = await ListModel.getDetailList(id);
+      const result = await ListModel.getDetailList(id, "true");
+      if (!result) {
+        throw {
+          code: 404,
+          message: "LIST_NOT_FOUND",
+        };
+      }
       return res.status(200).json({
         status: true,
         message: "GET_DETAIL_LIST_SUCCESS",
@@ -34,23 +40,35 @@ class List {
     }
   }
   async addList(req, res) {
-    try{
+    try {
       let uid_fk = req.jwt.id;
       let { wisata_fk, hotel_fk, transportation_fk, restaurant_fk } = req.body;
+      if (!wisata_fk) {
+        wisata_fk = null;
+      }
+      if (!hotel_fk) {
+        hotel_fk = null;
+      }
+      if (!transportation_fk) {
+        transportation_fk = null;
+      }
+      if (!restaurant_fk) {
+        restaurant_fk = null;
+      }
 
       const result = await ListModel.addList(
-        wisata_fk, 
-        hotel_fk, 
+        wisata_fk,
+        hotel_fk,
         transportation_fk,
         uid_fk,
-        restaurant_fk, 
+        restaurant_fk
       );
       return res.status(200).json({
         status: true,
         message: "ADD_LIST_SUCCESS",
         data: result,
       });
-    }catch(err){
+    } catch (err) {
       console.log(err);
       return res.status(err.code || 500).json({
         status: false,
--- src/models/DataModel.js
+++ src/models/DataModel.js
@@ -1,47 +1,49 @@
 const db = require("../db/db.js");
 
 class DataModel {
-    static async getHotel() {
-        const sql = `select
+  static async getHotel() {
+    const sql = `select
         hotel.id, 
         hotel.hotel_name, 
         hotel.address, 
         hotel_category.name as category_hotel, 
+        hotel_price.name as bintang,
         hotel_price.min_price, 
         hotel_price.max_price 
         FROM hotel JOIN hotel_category on hotel.hotel_category = hotel_category.id 
         JOIN hotel_price ON hotel.hotel_star = hotel_price.id `;
-        const result = await db.query(sql);
-        return result[0]
-    }
-    static async getDetailHotel(id) {
-        const sql = `select
+    const result = await db.query(sql);
+    return result[0];
+  }
+  static async getDetailHotel(id) {
+    const sql = `select
         hotel.id, 
         hotel.hotel_name, 
         hotel.address, 
-        hotel_category.name as category_hotel, 
+        hotel_category.name as category_hotel,
+        hotel_price.name as bintang, 
         hotel_price.min_price, 
         hotel_price.max_price 
         FROM hotel JOIN hotel_category on hotel.hotel_category = hotel_category.id 
         JOIN hotel_price ON hotel.hotel_star = hotel_price.id 
         where hotel.id = ${id}`;
-        const result = await db.query(sql);
-        return result[0]
-    }
+    const result = await db.query(sql);
+    return result[0];
+  }
 
-    static async getDestination() {
-        const sql = `select * from tempat_wisata where city = "bandung"`;
-        const result = await db.query(sql);
-        return result[0]
-    }
-    static async getDetailDestination(id) {
-        const sql = `select * from tempat_wisata where city = "bandung" AND id = ${id}`;
-        const result = await db.query(sql);
-        return result[0]
-    }
+  static async getDestination() {
+    const sql = `select * from tempat_wisata where city = "bandung"`;
+    const result = await db.query(sql);
+    return result[0];
+  }
+  static async getDetailDestination(id) {
+    const sql = `select * from tempat_wisata where id = ${id}`;
+    const result = await db.query(sql);
+    return result[0];
+  }
 
-    static async getRestaurant() {
-        const sql = `select 
+  static async getRestaurant() {
+    const sql = `select 
         restaurant.id, 
         restaurant.resto_name, 
         restaurant.address, 
@@ -50,12 +52,12 @@ class DataModel {
         category_resto.max_price, 
         restaurant.latitude, 
         restaurant.longtitude 
-        FROM restaurant JOIN category_resto ON restaurant.category = category_resto.id;`;
-        const result = await db.query(sql);
-        return result[0]
-    }
-    static async getDetailRestaurant(id) {
-        const sql = `select 
+        FROM restaurant JOIN category_resto ON restaurant.category = category_resto.id`;
+    const result = await db.query(sql);
+    return result[0];
+  }
+  static async getDetailRestaurant(id) {
+    const sql = `select 
         restaurant.id, 
         restaurant.resto_name, 
         restaurant.address, 
@@ -66,8 +68,8 @@ class DataModel {
         restaurant.longtitude 
         FROM restaurant JOIN category_resto ON restaurant.category = category_resto.id
         where restaurant.id = ${id}`;
-        const result = await db.query(sql);
-        return result[0]
-    }
+    const result = await db.query(sql);
+    return result[0];
+  }
 }
 module.exports = DataModel;
