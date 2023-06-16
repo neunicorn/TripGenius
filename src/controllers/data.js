8ce7ebd366768887f374f3bdf11e5f78a0e2e5bd
Zhafranath <zhafran.athofani23@gmail.com>
2023-06-16 12:53:58 +0700

    Change API Data

--- src/controllers/data.js
+++ src/controllers/data.js
@@ -4,9 +4,9 @@ const ImageHelper = require("../helpers/ImageHelper.js");
 class Data {
   async getDestination(req, res) {
     try {
-      const { page } = req.body;
+      const { page } = req.params;
       const OFFSET = (page - 1) * 10;
-      const result = await DataModel.getDestination();
+      const result = await DataModel.getDestination(OFFSET);
       const data = result.map((item) => {
         return {
           id: item.id,
@@ -68,9 +68,9 @@ class Data {
 
   async getHotel(req, res) {
     try {
-      const { page } = req.body;
+      const { page } = req.params;
       const OFFSET = (page - 1) * 10;
-      const result = await DataModel.getHotel();
+      const result = await DataModel.getHotel(OFFSET);
 
       return res.status(200).json({
         status: true,
@@ -105,9 +105,9 @@ class Data {
 
   async getRestaurant(req, res) {
     try {
-      const { page } = req.body;
+      const { page } = req.params;
       const OFFSET = (page - 1) * 10;
-      const result = await DataModel.getRestaurant();
+      const result = await DataModel.getRestaurant(OFFSET);
 
       return res.status(200).json({
         status: true,
@@ -164,7 +164,6 @@ class Data {
     try {
       const { id } = req.body;
       const result = await DataModel.getDestinationById(id);
-
       return res.status(200).json({
         status: true,
         message: "GET_DATA_SUCCESS",
@@ -178,6 +177,7 @@ class Data {
       });
     }
   }
+
 }
 
 module.exports = new Data();
